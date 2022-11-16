ac9a55ac855d28335265e92136496a28a3742dbf
Timur <timuryuta@gmail.com>
2022-11-17 01:28:06 +0300

    Added all Tasks and Homework with HomeworkTest

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,36 +1,65 @@
 package ru.gb.jseminar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Homework {
 
     // Пусть дан произвольный список целых чисел
     public static void main(String[] args) {
-
+        Homework hw = new Homework();
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 3, 8, 9, 4, 7, 2));
+        System.out.println(hw.removeNegativeValue(list));
+        System.out.println(hw.getMin(list));
+        System.out.println(hw.getMax(list));
+        System.out.println(hw.getAverage(list));
     }
 
     // Нужно удалить из него четные числа
     public List<Integer> removeNegativeValue(List<Integer> list){
-
-        return new ArrayList<>();
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i) % 2 == 0){
+                list.remove(i);
+            }
+        }
+        return list;
     }
 
     // Найти минимальное значение
     public Integer getMin(List<Integer> list){
-
-        return 0;
+        int minElement = 0;
+        if (list.size() > 0){
+            minElement = list.get(0);
+            for (int item: list){
+                if (item < minElement){
+                    minElement = item;
+                }
+            }
+        }
+        return minElement;
+//        return Collections.min(list);
     }
 
     // Найти максимальное значение
     public Integer getMax(List<Integer> list){
-
-        return 0;
+        int maxElement = 0;
+        if (list.size() > 0){
+            maxElement = list.get(0);
+            for (int item: list){
+                if (item > maxElement){
+                    maxElement = item;
+                }
+            }
+        }
+        return maxElement;
+//        return Collections.max(list);
     }
 
     // Найти среднее значение
     public Integer getAverage(List<Integer> list){
-
-        return 0;
+        int sum = 0;
+        for (int item: list){
+            sum += item;
+        }
+        return sum / 2;
     }
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,68 @@
 package ru.gb.jseminar;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class HomeworkTest {
 
+
+    @Test
+    void removeNegativeValue() {
+        // исходное
+        List<Integer> expectedlist = new ArrayList<>(Arrays.asList(1, 3, 8, 9, 4, 7, 2));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 3, 9, 7));
+
+        // делаем
+        Homework homework = new Homework();
+        List<Integer> actual = homework.removeNegativeValue(expectedlist);
+
+        // проверяем
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getMin() {
+        // исходное
+        List<Integer> expectedlist = new ArrayList<>(Arrays.asList(1, 3, 8, 9, 4, 7, 2));
+        int expected = 1;
+
+        // делаем
+        Homework homework = new Homework();
+        int actual = homework.getMin(expectedlist);
+
+        // проверяем
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getMaxSuccessful() {
+        // исходное
+        List<Integer> expectedlist = new ArrayList<>(Arrays.asList(1, 3, 8, 9, 4, 7, 2));
+        int expected = 9;
+
+        // делаем
+        Homework homework = new Homework();
+        int actual = homework.getMax(expectedlist);
+
+        // проверяем
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAverage() {
+        // исходное
+        List<Integer> expectedlist = new ArrayList<>(Arrays.asList(1, 3, 8, 9, 4, 7, 2));
+        int expected = 17;
+
+        // делаем
+        Homework homework = new Homework();
+        int actual = homework.getAverage(expectedlist);
+
+        // проверяем
+        Assertions.assertEquals(expected, actual);
+    }
 }
